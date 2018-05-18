da8d405cdf2b6236a7749ebe7119be2f00ddc73b
Naruto Yang <yangxiaocheng@eims.com.cn>
2018-05-18 14:41:47 +0800

    添加圆角按钮背景，调整主页面的按钮

--- app/src/main/java/com/naruto/mycontacts/Activities/MainActivity.java
+++ app/src/main/java/com/naruto/mycontacts/Activities/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//455898
+        setContentView(R.layout.activity_main);
     }
 }
